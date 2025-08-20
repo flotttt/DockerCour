fa040e0383c312adf117312d2e19bb89841c27ac
flotttt <thubin.florian@gmail.com>
2025-08-20 14:21:50 +0200

    tp 20

--- biblioflow-backend/src/app.module.ts
+++ biblioflow-backend/src/app.module.ts
@@ -1,12 +1,31 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { Book } from './books/entities/book.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule, BooksModule],
+  imports: [
+    // ✅ AJOUT : Configuration TypeORM
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.NODE_ENV === 'production' ? 'postgres' : 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'biblioflow_postgres_secure_password_2024',
+      database: 'biblioflow',
+      entities: [Book], // ✅ Importez vos entités ici
+      synchronize: true, // ✅ Auto-création des tables
+      logging: true, // ✅ Pour voir les requêtes SQL
+    }),
+    AuthModule,
+    UsersModule,
+    BooksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
